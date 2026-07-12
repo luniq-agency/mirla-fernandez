a85a1972a86fc1e5f908d8d5f86f3e188c006bb6
LUNIQ Agency <hello@luniq.agency>
2026-07-12 23:20:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default function Home() {
     <main>
       <section className="section-hero">
         <video
+          autoPlay
           loop
           muted
           className="video-hero"
